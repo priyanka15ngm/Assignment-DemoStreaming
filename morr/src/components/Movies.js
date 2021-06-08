612cec954b8159b94acc6e24c5df945d677954a5
priyanka nigam <priyanka.nigam_cs18@gla.ac.in>
2021-06-08 22:09:23 +0530

    Movies Component

--- morr/src/components/Movies.js
+++ morr/src/components/Movies.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import mov from "./mov.jpeg";
-function Movies(){
+import mov from "./mov.jpg";
+import Header from "./Header";
+function Movies({title}){
     return<>
-    <div className = "image">
+
+    <Header title={`MoviesList`}/>
+    <div className = "body">
         <img src = {mov} alt="series image" height ={200}/>
         <img src = {mov} alt="series image" height ={200}/>
         <img src = {mov} alt="series image" height ={200}/>
         <img src = {mov} alt="series image" height ={200}/>
+        
     </div>
     </>
 }
