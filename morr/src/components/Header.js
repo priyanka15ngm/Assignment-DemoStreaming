0bfd20721908bf7bb499ba8f15a13ec5487287be
priyanka nigam <priyanka.nigam_cs18@gla.ac.in>
2021-06-08 21:58:05 +0530

    Header

--- morr/src/components/Header.js
+++ morr/src/components/Header.js
@@ -1,8 +1,32 @@
 import "../css/Header.css";
 
 
-function Header(){
-        return<h1>I am a header</h1>
+function Header({title}){
+        return<>
+                <div className = "heading">
+                        <div className = "left_heading">
+                        <h1>DEMO</h1>
+                        <h3>Streaming</h3>
+                        </div>
+                        
+                        <div className = "right_heading">
+                        <p>Login</p>
+                        <button class="btn">Start your free trial</button>
+                        </div>
+
+
+
+
+                </div>
+                <div className="second_heading">
+                        <p>Popular {title}</p>
+
+                </div>
+        
+        
+        
+        
+        </>
  }
 
 
