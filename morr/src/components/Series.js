774ab90ea8d1a6950f87f4d47f50da7f178ae06f
priyanka nigam <priyanka.nigam_cs18@gla.ac.in>
2021-06-08 22:09:41 +0530

    Series Component

--- morr/src/components/Series.js
+++ morr/src/components/Series.js
@@ -1,8 +1,12 @@
 import React from "react";
-import ser from "./ser.jpeg";
-function Series(){
+import ser from "./ser.jpg";
+import Header from "./Header";
+function Series({ title }){
     return<>
-    <div className = "image">
+    {/* <h3>{title}</h3> */}
+    <Header title={`SeriesList`}/>
+    <div className = "body">
+        
         <img src = {ser} alt="series image" height ={200}/>
         <img src = {ser} alt="series image" height ={200}/>
         <img src = {ser} alt="series image" height ={200}/>
