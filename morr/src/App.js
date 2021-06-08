93a696b2472dee84e83ef06e16fa87148a719938
priyanka nigam <priyanka.nigam_cs18@gla.ac.in>
2021-06-08 22:12:19 +0530

    App Component

--- morr/src/App.js
+++ morr/src/App.js
@@ -1,7 +1,6 @@
 import Home from "./components/Home";
 import Movies from "./components/Movies";
 import Series from "./components/Series"; 
-import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
