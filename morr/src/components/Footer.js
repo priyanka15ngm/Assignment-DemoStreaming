0db31fbb7e7f1ad763ffcbb6be58144eaf5d7891
priyanka nigam <priyanka.nigam_cs18@gla.ac.in>
2021-06-08 21:57:29 +0530

    Added Footer

--- morr/src/components/Footer.js
+++ morr/src/components/Footer.js
@@ -1,6 +1,41 @@
 import "../css/Footer.css";
+import { FaFacebookF, FaTwitter,FaInstagram} from "react-icons/fa";
+import MobileStoreButton from 'react-mobile-store-button';
+
+
+
+const iOSUrl = 'https://itunes.apple.com/us/app/all-of-the-lights/id959389722?mt=8';
+// const name = "priyanka";
 function Footer(){
-    return<h3>I am a footer</h3>
+    
+    return<>
+            <div className="footer">
+                <div className = "foot" >
+                Home | Terms and condition | Privacy Policy | Collection Statement | Help | Manage Account<br>
+                </br>Copyright@Demo Streaming Priyanka Nigam 2021
+
+                <div class="bottom">
+                <FaFacebookF/>&nbsp;
+             <FaTwitter/>&nbsp;
+             <FaInstagram/>
+                </div>
+                 </div>
+                 <div class ="icons">
+             
+                     <MobileStoreButton store="ios" url={iOSUrl} linkProps={{ title: 'iOS Store Button' }}/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                     <MobileStoreButton store="ios" url={iOSUrl} linkProps={{ title: 'iOS Store Button' }}/>
+             
+             
+             </div>
+
+
+
+            </div>
+    
+    
+    
+    
+    </>
 }
 
 
